531317f94a381c0838670886d512dd4b320f6e95
Josh <joshuadronyi@gmail.com>
2019-10-01 09:47:48 -0400

    Basic set up for the app. added folders for mvvm. Created a layout and connected everything. Added dependencies for recyclerview and constraint view

---
+++ app/src/main/java/com/example/shibeapplication/Utils/Constants.java
@@ -0,0 +1,6 @@
+package com.example.shibeapplication.Utils;
+
+public class Constants {
+    public static final String BASE_URL = "http://shibe.online";
+    public static final String SHIBE_PATH = "/api/shibes";
+}
